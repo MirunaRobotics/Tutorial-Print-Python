d8dcda35138bb620babb8c1932062abf38b7d080
Miruna Robotics <168479798+MirunaRobotics@users.noreply.github.com>
2024-05-15 16:11:17 +0200

    Add files via upload

---
+++ Tutorial.py
@@ -0,0 +1,58 @@
+# Hola! Este és un tutorial de la variable "print"!
+
+
+# Primero imprimiremos algo, por ejemplo, hola mundo.
+
+print("Hola Mundo!")        # Es importante que si es un texto lleve comillas
+
+
+# Ahora llega...✨Momento de creatividad!✨
+# Rellena esta variable print y pon un texto, luego, ejecuta el progama!
+
+print("(inserta el texto)")
+
+
+# Print con variables
+
+Hola = "Hello"    # Definimos "Hola" a "Helllo"
+
+print(Hola)
+
+# Imprimir varias cosas:
+
+Python = "Miruna"
+
+Youtube = "TikTok"
+
+print("Estoy viendo", Python, "en", Youtube) # Usamos las comas para "unir" variables, strings o otras cosas
+
+
+# Print de input
+
+tutorial = input("Que estás viendo?") # Al definir una variable con un input, nos preguntará el texto para saber a que equivale
+print("Estás viendo", tutorial) 
+
+
+# Ahora llega...✨Momento de creatividad!✨
+# Haz con input una orden de restaurante, luego, ejecuta el progama!
+
+# Es un ejemplo (postdata)
+
+print("""
+Bebidas:
+
+Agua Mineral - Refrescante y pura.
+Zumo de Naranja Natural - Exprimido al momento.
+Sangría Casera - Con frutas de temporada.
+      
+Para Comer:
+
+Tortilla Española - Con huevos de corral y patatas.
+Paella Valenciana - Con pollo, conejo y verduras de la huerta.
+Bocadillo de Jamón Ibérico - En pan con tomate.
+Gazpacho Andaluz - Servido frío con picatostes.
+""")
+
+Ejemplo1 = input("Que quieres de beber?")
+Ejemplo2 = input("Y de comer?")
+print("Total, vas a beber", Ejemplo1, "y vas a comer", Ejemplo2, ", oido cocina!")
